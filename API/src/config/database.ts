e5b05b4c9ff82e68f56592677afbd7da19603ff5
Paulo Carlos <paulocarlosfilho@gmail.com>
2026-09-12 12:17:10 -0300

    bug: Erro no docker
    - Adicionando o Postgres e removendo o localstack pois estava dando erro.
    
    - Alteração do database.ts e adicionando o postgres e removendo o localhost
    
    - Aletreando o docker-compose.yml e adicionando o postgres usnado o healthcheck

--- API/src/config/database.ts
+++ API/src/config/database.ts
@@ -10,7 +10,7 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: process.env.DB_HOST || "localhost",
+  host: process.env.DB_HOST || "postgres",
   port: Number(process.env.DB_PORT) || 5432,
   username: process.env.DB_USERNAME || "postgres",
   password: process.env.DB_PASSWORD || "postgres",
